84ed4fe31000faaabee38201d252439937a5f882
Захаров Денис <zdv.work@yandex.ru>
2024-02-19 21:05:47 +0600

    добавил вызовы чтобы не ругался линтер

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,28 @@ function extractNumber(param){
   }
   return parseInt(num, 10);
 }
+
+// Строка короче 20 символов
+checkLenght('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkLenght('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkLenght('проверяемая строка', 10); // false
+
+// Строка является палиндромом
+checkPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+checkPalindrome('ДовОд'); // true
+// Это не палиндром
+checkPalindrome('Кекс'); // false
+// Это палиндром
+checkPalindrome('Лёша на полке клопа нашёл '); // true
+
+extractNumber('2023 год'); // 2023
+extractNumber('ECMAScript 2022'); // 2022
+extractNumber('1 кефир, 0.5 батона'); // 105
+extractNumber('агент 007'); // 7
+extractNumber('а я томат'); // NaN
+extractNumber(2023); // 2023
+extractNumber(-1); // 1
+extractNumber(1.5); // 15
